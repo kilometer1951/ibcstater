0824e7f86388fbd65f8a5c55a340de6de8b650e9
Serge Shalo <sergeshalowilson@gmail.com>
2020-10-23 12:26:02 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,2 +1,35 @@
+/**
+ * Plugin to validate unique array items by props
+ * @param {import("ajv").Ajv} ajv
+ */
+export function AjvPluginUniqueItemsByProperty(ajv) { //eslint-disable-line id-length
+    /* istanbul ignore next */
+    function validateUniqueItemsByProperty(
+        schema,
+        data,
+        parentSchema,
+        dataPath
+    ) {
+        validateUniqueItemsByProperty.errors = validateUniqueItemsByProperty.errors || [];
+        const hasDuplicates = data.map(data => data[schema])
+            .some((item, index, all) => all.indexOf(item) !== index); //eslint-disable-line id-length
+        if (hasDuplicates) {
+            validateUniqueItemsByProperty.errors.push({
+                keyword: 'uniqueItemsByProp',
+                dataPath,
+                message: 'must have unique items'
+            });
+        }
+        return !hasDuplicates;
+    }
 
-test
+    ajv.addKeyword('uniqueItemsByProperty', {
+        type: 'array',
+        errors: true,
+        metaSchema: {
+            type: 'string',
+            additionalProperties: false
+        },
+        validate: validateUniqueItemsByProperty
+    });
+}

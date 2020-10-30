080c28bf184cabea9b600fba57e3fd1a776b541b
Serge Shalo <sergeshalowilson@gmail.com>
2020-10-30 14:36:09 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,108 @@
-test
+"comma-dangle": [
+            "error",
+            "never"
+        ],
+        "curly": "error",
+        "dot-notation": "error",
+        "eqeqeq": "error",
+        "id-length": [
+            "error",
+            {
+                "min": 4,
+                "max": 50,
+                "exceptions": [
+                    "app",
+                    "req",
+                    "res",
+                    "url",
+                    "use",
+                    "pid",
+                    "Id",
+                    "id",
+                    "_id",
+                    "URL",
+                    "cpu",
+                    "log",
+                    "min",
+                    "max",
+                    "tag",
+                    "key",
+                    "fs",
+                    "by",
+                    "on",
+                    "let",
+                    "as",
+                    "$eq",
+                    "$in",
+                    "$ne",
+                    "$or"
+                ]
+            }
+        ],
+        "no-cond-assign": [
+            "error",
+            "always"
+        ],
+        "max-statements-per-line": [
+            "error",
+            {
+                "max": 1
+            }
+        ],
+        "no-console": "error",
+        "no-constant-condition": "error",
+        "no-debugger": "error",
+        "no-empty-function": "error",
+        "no-extra-bind": "error",
+        "no-implicit-globals": "error",
+        "no-loop-func": "error",
+        "no-magic-numbers": [
+            "warn",
+            {
+                "ignore": [
+                    0
+                ]
+            }
+        ],
+        "no-multi-spaces": "error",
+        "no-var": "error",
+        "no-self-compare": "error",
+        "no-useless-return": "error",
+        "no-trailing-spaces": "error",
+        "no-unused-vars": "error",
+        "no-alert": "error",
+        "no-lone-blocks": "error",
+        "no-extra-semi": "error",
+        "no-unreachable": "error",
+        "no-multiple-empty-lines": [
+            "error",
+            {
+                "max": 1,
+                "maxBOF": 0,
+                "maxEOF": 1
+            }
+        ],
+        "prefer-const": "error",
+        "prefer-template": "error",
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "sort-imports": [
+            "error",
+            {
+                "ignoreCase": true,
+                "ignoreMemberSort": false,
+                "memberSyntaxSortOrder": [
+                    "none",
+                    "single",
+                    "all",
+                    "multiple"
+                ]
+            }
+        ],
+        "sort-vars": "off"

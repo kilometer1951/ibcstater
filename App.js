7657e40ac08b19677a1a8ddf0ab931c0139cc94e
Serge Shalo <sergeshalowilson@gmail.com>
2020-11-10 22:28:05 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,20 @@
-test
+{
+  "testRegex": "((\\.|/*.)(test))\\.(jsx|js)?$",
+  "collectCoverageFrom": ["src/**/*.js", "src/**/*.jsx"],
+  "setupFilesAfterEnv": ["<rootDir>/src/jest.setup.js"],
+
+  "moduleNameMapper": {
+    "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+    "\\.(gif|ttf|eot|svg|png|ico)$": "<rootDir>/__mocks__/fileMock.js",
+    "^/deployments/config/app\\.json$": "<rootDir>/__mocks__/fileMock.js"
+  },
+  "coverageThreshold": {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": 80
+    }
+  },
+  "coverageDirectory": "./test/unit/reports/coverage"
+}

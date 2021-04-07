503697dbdd59c60aadb85f45ddb31cbce3046a85
Serge Shalo <sergeshalowilson@gmail.com>
2021-04-07 10:50:52 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,1 @@
 
- "@babel/cli": "^7.12.10",
-    "@babel/core": "^7.12.10",
-    "@babel/plugin-transform-regenerator": "^7.12.1",
-    "@babel/preset-env": "^7.12.11",
-    "@babel/preset-react": "^7.12.10",
-    "babel-cli": "^6.26.0",
-    "cross-env": "^7.0.3",
-    "react": "^17.0.1",
-    "react-dom": "^17.0.1",
-    "react-scripts": "4.0.1"
